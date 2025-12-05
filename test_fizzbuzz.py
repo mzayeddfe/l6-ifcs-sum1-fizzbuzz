d4fdf846cbd00566a22f8b6385b4578a653b5ac5
Menna <menna.zayed@education.gov.uk>
2025-12-05 14:28:12 +0000

    Write fizzbuzz test (#2)
    * added fizzbuzz function
    
    * created pytest for fizbuzz

---
+++ test_fizzbuzz.py
@@ -0,0 +1,35 @@
+# import the fizzbuzz function from the fizzbuzz script 
+
+from fizzbuzz import fizzbuzz
+import pytest
+
+
+# start putting the tests into functions 
+
+# test the basic functionality of the fizzbuzz function 
+
+
+def test_fizzbuzz_basics():
+    assert fizzbuzz(5) == [1,2,"fizz",4,"buzz"]
+
+
+# test the 0 error
+
+def test_fizzbuzz_zero_error():
+    #import pytest 
+    with pytest.raises(ValueError):
+        fizzbuzz(0)
+
+# test the negative value error
+
+def test_fizzbuzz_negative():
+    #import pytest
+    with pytest.raises(ValueError):
+        fizzbuzz(-1)
+
+# test the error for the data type of num_entry 
+
+def test_fizzbuzz_type():
+    #import pytest
+    with pytest.raises(TypeError):
+        fizzbuzz("hello!")
