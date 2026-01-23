32945c2c9cfe391b8074da774a5ed5119ce84def
mzayeddfe <menna.zayed@education.gov.uk>
2026-01-23 13:04:14 +0000

    adding multiples of 3 feature

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -1,3 +1,6 @@
 def fizzbuzz(n):
+
+    if n % 3 == 0: 
+        return "Fizz"
     #return the input as strings
     return str(n)
--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -6,3 +6,8 @@ from fizzbuzz import fizzbuzz
 
 def test_return_as_string():
     assert fizzbuzz(1) == "1"
+
+#test function works for multiples of 3
+
+def test_multi_three():
+    assert fizzbuzz(3) == "Fizz"
