8221a87b892c9d70007a324ee7635b626d90ba2f
mzayeddfe <menna.zayed@education.gov.uk>
2026-01-23 16:15:33 +0000

    adding extra tests

--- test_fizzbuzz.py
+++ test_fizzbuzz.py
@@ -1,6 +1,7 @@
 # import fizzbuzz
 
 from fizzbuzz import fizzbuzz 
+import pytest
 
 # check the function returns numeric inputs as string
 
@@ -23,3 +24,15 @@ def test_multi_five():
 
 def test_multi_fifteen():
     assert fizzbuzz(15) == "Fizzbuzz"
+
+# start testing for errors
+
+#check that input is int
+def test_non_int_input():
+    with pytest.raises(TypeError):
+        fizzbuzz("hello!")
+
+
+def test_non_negative_input():
+    with pytest.raises(ValueError):
+        fizzbuzz(-1)
